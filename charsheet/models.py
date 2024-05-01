0dc113c2550ea87307855b269f6d16cc19be5fe5
Raval <ravalc05@gmail.com>
2024-05-01 16:32:04 -0400

    First commit of the project

---
+++ charsheet/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Character(models.Model):
+    char_name = models.CharField(max_length=50)
+    char_race = models.CharField(max_length=30)
+    char_class = models.CharField(max_length=30)
+    char_lvl = models.PositiveIntegerField()
+    background = models.CharField(max_length=30)
+    alignment = models.CharField(max_length=30)
+    char_str = models.IntegerField()
+    char_dex = models.IntegerField()
+    char_con = models.IntegerField()
+    char_int = models.IntegerField()
+    char_wis = models.IntegerField()
+    char_char = models.IntegerField()
