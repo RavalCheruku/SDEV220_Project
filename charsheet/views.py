863b5c626990f6bc33772e99ab594de647a35e6c
Raval <ravalc05@gmail.com>
2024-05-02 19:15:01 -0400

    HTML and views

--- charsheet/views.py
+++ charsheet/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import Character
 
 # Create your views here.
-def char_list(request):    
-    return render(request, 'charsheet/char_list.html', {})
+def char_list(request):
+    char = Character.objects.order_by('char_name')    
+    return render(request, 'charsheet/char_list.html', {'char':char})
 
 def get_data(request):
     my_data = Character.objects.all()
